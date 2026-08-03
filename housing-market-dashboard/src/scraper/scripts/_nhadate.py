8431148e774ffa554219e1f4f28ae2fd4815d8d2
TA DUC DUY <99133073+ducduyhsme@users.noreply.github.com>
2026-08-03 22:35:23 +0700

    .

--- housing-market-dashboard/src/scraper/scripts/_nhadate.py
+++
@@ -1,5 +0,0 @@
-from src.scraper.selenium_scraper import render_listing_cards
-xs=render_listing_cards('https://nhadat24h.net/ban-can-ho-chung-cu','.dv-item .dv-txt a[href*="-ID"]','.dv-item')
-for x in xs[5:10]:
- print('---')
- print(x['text'][:500])
